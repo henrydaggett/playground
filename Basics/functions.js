f8beb7498bef584f1502f16e2585a069200823c1
Henry Daggett <34757031+henrydaggett@users.noreply.github.com>
2020-12-19 12:14:02 +0000

    functions learning

--- Basics/functions.js
+++ Basics/functions.js
@@ -29,3 +29,49 @@ const doctorise2 = function (firstName) {
 // };
 
 const inchToCm = (inches) => inches * 2.54;
+
+// if you have more than one param in your function then you need to use brackets arounf the params
+
+function add(a, b = 3) {
+  const total = a + b;
+  return total;
+}
+
+// with arrow functions the name is inferred from the name of the variable, so in the below example you will call total(a,b) to get the function
+
+const total = (a, b = 3) => a + b;
+
+// function makeABaby(first, last) {
+//   const baby = {
+//     name: `${first} ${last}`,
+//     age: 0,
+//   };
+//   return baby;
+// }
+
+const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
+
+// IIFE = Immidiately invoked Fuction Expression
+
+// (function (age) {
+//   return 'you are cool and `${age}`';
+// })(10);
+
+// Methods
+const henry = {
+  name: 'Henry Daggett',
+  // this is a method: (had to comment it because eslint corrects it)
+  // sayHi: function () {
+  //   console.log(`Hi ${this.name}`);
+  // },
+  // Object short hand method:
+  yellHi() {
+    console.log('HI HENRY');
+  },
+  // Arrow function method:
+  whisperHi: () => {
+    console.log('hi henry');
+  },
+};
+
+// Callback functions = something that will happen when something is done
